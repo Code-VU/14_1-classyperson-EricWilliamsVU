5616fa69a31f21fd48e087de07a246328de99982
Eric Williams <eric.williams@vu.com>
2023-05-25 20:12:22 -0500

    Okie dokie

--- classy_person.py
+++ classy_person.py
@@ -6,10 +6,19 @@ don't forget the use of 'self' and to have the methods:
 3. say_greeting
 4. count_to_age
 '''
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-class Person:
+    def increase_age(self):
+        self.age += 1
 
+    def say_greeting(self):
+        print("Hello world! My name is", self.name + "!")
 
-
+    def count_to_age(self):
+        for i in range(1, self.age + 1):
+            print(i)
 
 # You won't need to call anything here.
